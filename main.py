e50a3c5f299921f933d80b53a3dde9f2c7e38861
ristovski311 <ristovski311@gmail.com>
2025-12-24 18:50:38 +0100

    Zapocet rad na reprezentaciji stanja.

---
+++ main.py
@@ -0,0 +1,8 @@
+from state.game_state import *
+from config.game_config import *
+from gameplay.game_moves import *
+from gui.game_display import *
+
+
+state = create_initial_state(5)
+print(state)
--- state/game_state.py
+++ state/game_state.py
@@ -2,46 +2,43 @@
 # Funkcije za stanja
 #
 
-# mock-up stanje 
-# prvo pristupamo stanju preko "indeksa" slova state[A]
-# tako dobijamo listu polja koja su oznacena sa A, odozgo na dole
-# nakon toga jos jednim indeksiranjem toga nekim brojem (broj ide od 1 pa do nekog broja)
-# dobijemo konkretno polje, na primer state[A][1] je prvo polje na vrhu kolone A
-# na taj nacin dobijamo mogucnost da imamo polje state[A][0] koje je obojeno inicijalno polje koje pripada nekom igracu.
-# dodavanjem "LEFT" i "RIGHT" kolona mozemo da dodamo i one 2 samo obojene kolone skroz levo i skroz desno
-# koje predstavljaju ostrvca igraca
 
-### Mora da se razmisli jer ne krecu sve kolone od indeksa 1 ^^^
-### slovo na polovini table ima indekse od 0, ali posle toga indeksi se smanjuju
-
-
-# Ove liste sadrze tuple-ove tipa (stanje_polja,). Namerno ga ostavljamo da bude tuple ako u buducnosti moramo da dodamo
-# novu informaciju u polje 
-# Sva polja u levoj i desnoj koloni su tipa (RED_ili_GREEN, 0), dakle ili pripadaju crvenom ili zelenom igracu a duzina puta je 0 njima
-
-game_state = {
-    'LEFT': [()],
-    'A': [()],
-    'B': [],
-    'C': [],
-    'D': [],
-    'E': [],
-    'F': [],
-    'G': [],
-    'H': [],
-    'I': [],
-    'RIGHT': []
-}
 
 # Definisanje stanja TODO
 
-    #Stanje 
+# Stanje - graf
+# Kljucevi su tuple objekti sa koordinatama tog polja u tabli (A, 1) je na primer u koloni A prvo polje za igru
+# Polja koja su hardcode ostrva su recimo (A, 0), (B, 0)... ali i cela LEFT i RIGHT kolona
+# Od sredisnjeg slova (ako su slova od A do I, to je slovo E, pogledaj sliku na slajdovima) krece drugacije indeksiranje
+# svako slovo posle sredisnjeg ima pocetni indeks za 1 veci od prethodnog
+# pa tako F ima prvo polje (F, 2), G ima (G, 3) - ovo su polja na kojima korisnik ima pravo da satavi kamen
+# a polja koja su hardcode ostrva su idalje na vrhu kolone (open, bolje se vidi sa slike) -  (F, 1), (G, 2) ... 
 
+# Svaki kljuc ima tuple sa 2 stvari -> prva je stanje polja, GREEN RED ili None (None je slobodno)
+# druga stvar je lista suseda tog polja, bice nam lakse za search kasnije, verovatno A*
+
+# mockup:
+
+# graph = {
+#     ('A',1): ('RED', [('A', 0), ('A', 2), ('B', 1), ('B', 2), ('LEFT', 1)]),
+#     ('A',2): (None, [('A', 1), ('A', 3), ('B', 2), ('B', 3), ('LEFT', 2)]),
+#     ...
+#     ('B', 1) : (None, [...])
+# }
+
+game_state = dict()
 
 # Funkcija za pravljenje inicijalnog stanja TODO
 
-def create_initial_state():
-    print("Kreiranje pocetnog stanja!")
+def create_initial_state(board_size: int):
+    # Ovo nam odredjuje broj kolona i broj "vrsta"
+    board_diameter = 2 * board_size - 1
+    column_letters = [chr(ord('A') + i) for i in range(board_diameter)]
+    column_letters = ['LEFT'] + column_letters + ['RIGHT']
+    game_state = {
+        (let, num): (None, []) for let in column_letters for num in range(board_diameter)
+    }
+    return game_state
     
 # Funkcija za prikaz proizvoljnog stanja TODO    
 
