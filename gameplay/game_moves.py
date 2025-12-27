c77c4ff15ea80eb8431b06d0036fe1e78dd2f65d
Tara Milijic <tarwmilijic@elfak.rs>
2025-12-27 18:39:04 +0100

    Dodate funkcije za unos poteza, provere validnosti, izbor pocetnih parametara

--- gameplay/game_moves.py
+++ gameplay/game_moves.py
@@ -5,14 +5,71 @@
 
 
 # Funkcija za ispravnost unosa poteza (slobodno dodati dodatne f-je ako su potrebne) TODO
-def is_move_valid():
-    print("Proverava da li je potez dozvoljen / ispravan.")
+def is_move_valid(game_state,position):
+    if position not in game_state:
+        return False
+
+    # 2. da li je slobodno
+    if game_state[position][0] is not None:
+        return False
+
+    return True
     
 
 # Funkcija za unos poteza TODO
-def input_a_move():
-    print("Omogucava korisniku da unese potez koji zeli da odigra, to je pozicija polja na koje zeli da stavi kamencic")
-    
+def input_a_move(game_state):
+    while True:
+        user_input = input("Unesi potez (npr. C 3): ").strip().upper()
+
+        try:
+            col, row = user_input.split()
+            row = int(row)
+            position = (col, row)
+        except ValueError:
+            print("Neispravan format.")
+            continue
+
+        if is_move_valid(game_state, position):
+            return position
+        else:
+            print("Potez nije dozvoljen. Polje ne postoji ili je zauzeto.")
+
+def choose_game_mode():
+    while True:
+        print("Izaberite režim igre:")
+        print("1 - Čovek protiv Čoveka")
+        print("2 - Čovek protiv Računara")
+        choice = input("Vaš izbor (1 ili 2): ").strip()
+
+        if choice == "1":
+            return "HUMAN_HUMAN"
+        elif choice == "2":
+            return "HUMAN_AI"
+        else:
+            print("Neispravan izbor. Pokušajte ponovo.")
+
+
+def choose_first_player():
+    while True:
+        choice = input("Ko igra prvi? (H = Human, A = AI): ").strip().upper()
+        if choice == "H":
+            return "HUMAN"
+        elif choice == "A":
+            return "AI"
+        else:
+            print("Neispravan izbor. Unesite H ili A.")
+
+def choose_first_symbol():
+    """
+    Omogućava izbor koji simbol ide prvi: X ili O
+    """
+    while True:
+        choice = input("Koji simbol igra prvi? (X/O): ").strip().upper()
+        if choice in ["X", "O"]:
+            return choice
+        else:
+            print("Neispravan izbor. Unesite X ili O.")
+
 
 ### !!! 
 # Nije potrebno praviti funkcije za odigravanje poteza.
--- main.py
+++ main.py
@@ -10,4 +10,13 @@ state = create_initial_state(board_size=game_config["board_size"])
 arb = create_arbitrary_state([('A', 3), ('B', 5)], [('E', 3), ('I', 5)])
 #display_graph(arb)
 
+game_mode = choose_game_mode()
+
+first_player_type = choose_first_player()
+
+first_symbol = choose_first_symbol()
+
+position = input_a_move(state)
+
 draw(state)
+
